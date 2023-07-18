4a6a40beb2392a9737add2ce15fdaf9c784e5bfa
fabioalves291 <fabioalvesdeandrade29@gmail.com>
2023-07-17 22:35:02 -0300

    alterado ocliente com tredrecv

--- clit_tcp.py
+++ clit_tcp.py
@@ -1,15 +1,24 @@
 #! /usr/bin/python3
-
+import threading
 import socket
 import datetime
 #from default import destino
+
+
 destino         = "localhost",2323
 clientS         = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 utf8            = "UTF-8"
 bollConexo      = False
-
 buffer          = 512
-# iniciando conexão
+
+def recvallmsg():
+    while True:
+        try:
+            data = clientS.recv(buffer)
+            print(data.decode(utf8))
+        except ConnectionRefusedError:
+            print("conexão recusada\n")
+
 while True:
     try:
         conn           = clientS.connect(destino)
@@ -17,24 +26,18 @@ while True:
         break
     except ConnectionRefusedError:
         print("conexão recusada\n")
-# enviado msg
 
-try:
-    data = clientS.recv(buffer)
-except ConnectionRefusedError:
-    print("conexão recusada\n")
 #criar uma tradding para ficar sempre ouvindo.! e mostrando mensagens recebidas pelo servidor
+
+# enviado msg
+thread = threading.Thread(target=recvallmsg)
+thread.start()
+
 while True:
-    print(data.decode(utf8))
     mensage = input(">>  ")
     if mensage =="":
         print("envie algo")
         continue
     clientS.send((mensage).encode(utf8))
-    try:
-        data = clientS.recv(buffer)
-    except ConnectionRefusedError:
-        print("conexão recusada\n")
-    
-    
+
     # ;) toda ação resulta numa reação
